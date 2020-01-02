40bca33db6b1a4e408c467c2efde0dde513aa821
Manish <manisharadwad@gmail.com>
2020-01-03 00:50:45 +0530

    Better Pattern

--- 10DaysOfCode/GaneshPattern.py
+++ 10DaysOfCode/GaneshPattern.py
@@ -1,27 +1,8 @@
 n = int(input())
-for i in range(n):
-    for j in range(n):
-        if i < n//2:
-            if j < n//2:
-                if j == 0:
-                    print('*',end='')
-                else:
-                    print(' ',end='')
-            elif j == n//2:
-                print('*',end='')
-            else:
-                if i == 0:
-                    print('*',end = '')
-        elif i == n//2:
-            print("*",end='')
-        else:
-            if j == n-1 or j == n//2:
-                print('*',end='')
-            elif i == n-1 :
-                if j<=n//2 or j == n-1:
-                    print('*',end='')
-                else:
-                    print(' ',end='')
-            else:
-                print(' ',end='')
-    print()
+print('*'+ ' ' * int((n-3)/2) + '*' * int((n+1)/2))
+for _ in range((n-3)//2 + 1):
+    print('*'+' '*int((n-3)/2)+'*'+' '*int((n-3)/2))
+print('*' * n)
+for _ in range((n-3)//2 + 1):
+    print(' '*int((n-1)/2)+'*'+' '*int((n-3)/2)+'*')
+print('*'*int((n+1)/2) + ' '*int((n-3)/2) + '*')
